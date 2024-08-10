a6ac0da1df99420c9a3239a9f1837fc0e40839b6
Het Prajapati <prajapatihet52@gmail.com>
2024-08-10 16:57:14 +0530

    third commit of het

--- JScribe/src/components/HelperHeader.tsx
+++ JScribe/src/components/HelperHeader.tsx
@@ -1,15 +1,44 @@
-import { Share2Icon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
+import { Save, Share2 } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function HelperHeader() {
   return (
     <div className="__helper_header h-[50px] bg-black text-white p-2 flex justify-between items-center">
       <div className="__btn_container flex gap-1">
-        <Button variant="sucess">Save</Button>
-        <Button variant="secondary">
-          <Share2Icon />
+        <Button
+          className="flex justify-center items-center gap-1"
+          variant="sucess"
+        >
+          <Save size={16} />
+          Save
+        </Button>
+        <Button
+          className="flex justify-center items-center gap-1"
+          variant="secondary"
+        >
+          <Share2 size={16} />
           Share
         </Button>
+      </div>
+      <div className="__tab_switcher flex justify-center items-center gap-1 ">
+        <small>Current Language: </small>
+        <Select defaultValue="html">
+          <SelectTrigger className="w-[120px] bg-gray-800 outline-none focus:ring-0 ">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="html">HTML</SelectItem>
+            <SelectItem value="css">CSS</SelectItem>
+            <SelectItem value="javascript">JavaScript</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
     </div>
   );
---
+++ JScribe/src/redux/slices/compilerSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const compilerSlice = createSlice({
+    name: "compilerSlice",
+    initialState,
+    reducers: {}
+})
+
+export default compilerSlice.reducer
