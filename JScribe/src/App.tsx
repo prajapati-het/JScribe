8a69035e882aff908b510181353f69a68780ed78
Het Prajapati <prajapatihet52@gmail.com>
2024-08-10 14:39:49 +0530

    Reapply "first commit"
    This reverts commit 586d8945893c757794e2e14c7b6ec33c25815ff0.

---
+++ JScribe/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis quam
+        quia fugiat. Dolorem accusantium ipsa ea cum sapiente totam officia sit
+        adipisci beatae veniam quidem, a, fuga similique modi animi.
+        <Button variant="default" >Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
