978552fbee87d35de22a5820884060ae0c14ca6c
Meet1512 <iconic1512@gmail.com>
2024-08-16 03:00:09 +0530

    remaining from model creation

--- server/src/controllers/complierController.ts
+++
@@ -1,8 +0,0 @@
-import { Request,Response } from "express";
-
-export const saveCode = (req:Request , res:Response) => {
-    try{
-    } catch(error){
-        return res.status(500).send({message:"Error saving code",error})
-    }
-};
--- server/src/routes/complierRouter.ts
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import { saveCode } from '../controllers/complierController';
-
-export const compileRouter = express.Router()
-compileRouter.post("/save",saveCode)
