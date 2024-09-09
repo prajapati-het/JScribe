0d783084e90690a99316a09d78fec9489cae0f10
Het Prajapati <prajapatihet52@gmail.com>
2024-09-09 14:03:37 +0530

    main

---
+++ JScribe/src/redux/slices/api.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { CompilerSliceStateType } from "./compilerSlice";
+
+export const api = createApi({
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:4000",
+    }),
+    endpoints: (builder) => ({
+        saveCode: builder.mutation<{ url: string; status: string }, CompilerSliceStateType["fullCode"]>({
+            query: (fullCode) => ({
+                url: "/compiler/save",
+                method: "POST",
+                body: fullCode,
+            }),
+        }),
+    }),
+});
+
+export const { useSaveCodeMutation } = api;
--- JScribe/src/redux/store.ts
+++ JScribe/src/redux/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import compilerSlice from "./slices/compilerSlice";
+import { api } from "./slices/api";
 
 export const store = configureStore({
-    reducer:{
+    reducer: {
+        [api.reducerPath]: api.reducer,
         compilerSlice,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>;
